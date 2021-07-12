a977d0753a23f5a3813bef96995ca2fcabd23fb0
vanduong0504 <vanduong0504@gmail.com>
2021-07-12 18:55:03 +0700

    Done

---
+++ README.md
@@ -0,0 +1,11 @@
+### Just a fun password checker project with [API](https://haveibeenpwned.com/Passwords).
+Enter your password as `input.txt` for more security. 
+
+Sample output:
+```
+Your password is ******. Found 24230577 times on the Internet!!!
+Your password is *****. Found 76 times on the Internet!!!
+Your password is ****. Found 380 times on the Internet!!!
+Your password is *************************. You can use your password on the Internet <3
+Your password is **************************. You can use your password on the Internet <3
+```
--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-if __name__ == "__main__":
-    print("yes")
-    print("testing Pycharm")
+import requests
+import hashlib
+
+
+def read_txt(file):
+    with open(file, mode='r') as file:
+        output = file.read().splitlines()
+    return output
+
+
+def convert_SHA1(string):
+    SHA1 = hashlib.sha1(string.encode()).hexdigest().upper()
+    return SHA1[0:5], SHA1[5:]
+
+
+def get_response(char):
+    url = 'https://api.pwnedpasswords.com/range/' + char
+    request = requests.get(url)
+    return [line.split(":") for line in request.text.splitlines()]
+
+
+def check(password_, response_):
+    for item in response_:
+        if password_ == item[0]:
+            return item[1]
+    return 0
+
+
+if __name__ == '__main__':
+    passwords = read_txt("input.txt")
+    for password in passwords:
+        first, last = convert_SHA1(password)
+        response = get_response(first)
+        time = check(last, response)
+        pw = f'Your password is {len(password)*"*"}.'
+        if time:
+            print(f'{pw} Found {time} times on the Internet!!!')
+        else:
+            print(f'{pw} You can use your password on the Internet <3')
